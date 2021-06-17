2445841250ae0c4183829fc70d3987602a07cd01
karolinacicha97 <karolinacicha97@gmail.com>
2021-06-17 22:22:04 +0200

    Dodanie odpowiedzi na pytania

--- src/mystrategy/Client.java
+++ src/mystrategy/Client.java
@@ -80,6 +80,31 @@ public class Client {
          System.out.println();
         System.out.println("------------------------------");
 //       
-       
+
+//Pytania:
+/*//1.	Proszę o podanie głównego celu zastosowania wzorca strategii.*/
+//Wzorzec pozwala na rozszerzanie funkcjonalności aplikacji poprzez możliwość 
+//zmiany realizacji algorytmu poprzez obiekty klasy. Program powinien zachowywać 
+//się w zależności od sytuacji. Pozwala to na unikanie nadmiernego stosowanie 
+//np. „ifów”. 
+
+/*//2.	Czy istnieje możliwość zmiany algorytmu w czasie działania programu?*/
+//Tak, umożliwia modyfikację. W przypadku stworzenia aplikacji jest możliwe 
+//stworzenie programu tak, aby było możliwe wybranie jakiego sortowania używamy, 
+//czyli jaką implementacje sortowania użyjemy. Możemy wybierać dzięki klasie 
+//Context. Klasa Context zależnie od zastanego warunku, inicjalizuje jedną z 
+//klas strategii. 
+
+/*//3.	 W jaki sposób wzorzec enkapsuluje poszczególne algorytmy?*/
+//Enkapsulacja jest zastosowana poprzez implementacje interfejsu ISortStrategy. 
+//Wyodrębnienie interfejsu, powoduje, że ukrywamy przed użytkownikiem kod klas 
+//wewnętrznych
+
+/*//4.	Kiedy stosować wzorzec strategii?*/
+//Używamy strategii wtedy, kiedy w kodzie wybieramy sposób wykonania jakiegoś 
+//algorytmu. Kiedy w zależności od danych/kontekstu wykorzystać odpowiedni 
+//proces z puli algorytmów (połączonych poprzez interfejs)
+//
+//       
     }
 }
